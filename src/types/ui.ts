61e32a5e263d03b3f42064c1a882f274b141334e
Elmore Shaw <31955678+elmoreshaw@users.noreply.github.com>
2025-06-08 15:24:51 +1000

    feat: subscript long zeros (#71)
    * feat: added subscript for long zeros in user balance
    
    * chore: rounded all token amounts to 4 decimal places
    
    ---------
    
    Co-authored-by: elmore-shaw <elmore.shaw@furo.io>

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -1,3 +1,9 @@
 export type Tab = "swap" | "earn" | "lending" | "dashboard";
 export type Theme = "light" | "dark";
 export type AvailableIconName = "Coins" | "Cable" | "Wallet" | "ArrowLeftRight";
+export interface FormattedNumberParts {
+  hasSubscript: boolean;
+  subscriptCount: number;
+  remainingDigits: string;
+  originalValue: string;
+}
