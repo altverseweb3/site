c99d40dd2991cd2cade8be8157dca12f971605be
elmoreshaw <elmoreshaws@gmail.com>
2025-07-16 21:25:42 +1000

    feat: added types for swap history querying

--- src/types/web3.ts
+++ src/types/web3.ts
@@ -423,3 +423,57 @@ export interface UserWallets {
   solana?: string;
   sui?: string;
 }
+
+export interface SwapData {
+  trader: string;
+  sourceTxHash: string;
+  sourceChain: string;
+  swapChain: string;
+  destChain: string;
+  fromAmount: string;
+  fromTokenAddress: string;
+  fromTokenChain: string;
+  fromTokenSymbol: string;
+  fromTokenLogoUri: string;
+  fromTokenPrice: number;
+  toTokenPrice: number;
+  toTokenAddress: string;
+  toTokenChain: string;
+  toTokenSymbol: string;
+  toTokenLogoUri: string;
+  destAddress: string;
+  status: string;
+  clientStatus: string;
+  initiatedAt: string;
+  toAmount: string;
+  stateAddr: string;
+  service: string;
+  statusUpdatedAt: string;
+  auctionMode: number;
+  referrerBps: number;
+  mayanBps: number;
+  orderHash: string;
+  cctpNonce: string | null;
+  fulfillTxHash: string;
+  refundTxHash: string | null;
+  clientRelayerFeeRefund: number;
+  orderId: string;
+}
+
+export interface SwapResponse {
+  data: SwapData[];
+  metadata: {
+    count: number;
+    volume: number;
+  };
+}
+
+export interface SwapQueryResult {
+  referrerAddress: string;
+  traderAddress: string;
+  response: SwapResponse;
+  rawResponse?: Response;
+  error?: string;
+}
+
+export type ChainType = "EVM" | "SOL" | "SUI";
