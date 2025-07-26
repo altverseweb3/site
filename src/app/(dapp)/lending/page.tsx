3137a344e61d308da562ada7d3d8ebea6a9c01c2
Elmore Shaw <31955678+elmoreshaw@users.noreply.github.com>
2025-07-26 11:59:47 +1000

    bugfix: wallet infinite loop (#178)
    * chore: fixing wallet issue WIP
    
    * bugfix: no more infinite loop loading aave reserves
    
    * chore: copied wallet disclaimer from earn

--- src/app/(dapp)/lending/page.tsx
+++ src/app/(dapp)/lending/page.tsx
@@ -4,30 +4,53 @@ import BorrowComponent from "@/components/ui/lending/BorrowComponent";
 import PoweredByAave from "@/components/ui/lending/PoweredByAave";
 import SupplyBorrowMetricsHeaders from "@/components/ui/lending/SupplyBorrowMetricsHeaders";
 import SupplyComponent from "@/components/ui/lending/SupplyComponent";
+import WalletConnectButton from "@/components/ui/WalletConnectButton";
 import React, { useState, useEffect } from "react";
-import { useSetActiveSwapSection } from "@/store/web3Store";
+import {
+  useSetActiveSwapSection,
+  useIsWalletTypeConnected,
+} from "@/store/web3Store";
+import { WalletType } from "@/types/web3";
 
 const BorrowLendComponent: React.FC = () => {
   const [activeTab, setActiveTab] = useState("borrow");
   const setActiveSwapSection = useSetActiveSwapSection();
+  const isWalletConnected = useIsWalletTypeConnected(WalletType.REOWN_EVM);
 
   useEffect(() => {
     setActiveSwapSection("lend");
   }, [setActiveSwapSection]);
 
   return (
-    <div className="flex h-full w-full items-start justify-center sm:pt-[6vh] pt-[2vh] min-h-[500px]">
-      <div className="w-full">
-        <SupplyBorrowMetricsHeaders
-          activeTab={activeTab}
-          onTabChange={setActiveTab}
-        />
-        {activeTab === "supply" ? <SupplyComponent /> : <BorrowComponent />}
+    <div className="w-full">
+      {isWalletConnected ? (
+        <>
+          <SupplyBorrowMetricsHeaders
+            activeTab={activeTab}
+            onTabChange={setActiveTab}
+          />
+          {activeTab === "supply" ? <SupplyComponent /> : <BorrowComponent />}
 
-        <PoweredByAave />
-      </div>
+          <PoweredByAave />
+        </>
+      ) : (
+        <>
+          <div className="text-center py-16 md:py-24 px-4 md:px-8">
+            <p className="text-zinc-400 mb-6 px-2 sm:px-8 md:px-16 lg:px-20 text-sm md:text-lg max-w-3xl mx-auto leading-relaxed">
+              please connect an EVM wallet (metamask, etc.) to view and manage
+              your positions.
+            </p>
+            <div className="flex justify-center">
+              <WalletConnectButton
+                className="w-1/4 text-center rounded-lg"
+                size="lg"
+                walletType={WalletType.REOWN_EVM}
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
-
 export default BorrowLendComponent;
