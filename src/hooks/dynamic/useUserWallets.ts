47793deca7acfc4311d297b8ed405d3616a753d4
Elmore Shaw <31955678+elmoreshaw@users.noreply.github.com>
2025-10-11 15:48:17 +1100

    feat: corrected connect wallet button text in header (#414)
    * fix: revert connect wallet modal disclaimer due to react mismatch (#406)
    
    * feat: apply disclaimer (#407)
    
    * chore: rename WalletConnectButton to ConnectWalletButton
    
    * feat: apply disclaimer before wallet connections
    
    * feat: corrected connect wallet button text in header
    
    * chore: updated destination wallet fetch
    
    ---------
    
    Co-authored-by: machineboyy <78896694+machineboyy@users.noreply.github.com>

---
+++ src/hooks/dynamic/useUserWallets.ts
@@ -0,0 +1,48 @@
+import {
+  useUserWallets as useDynamicUserWallets,
+  Wallet,
+} from "@dynamic-labs/sdk-react-core";
+import { useSourceChain, useDestinationChain } from "@/store/web3Store";
+import { WalletType } from "@/types/web3";
+
+export const useConnectedRequiredWallet = (): boolean => {
+  const userWallets = useDynamicUserWallets();
+  const requiredWalletType = useSourceChain().walletType;
+  if (
+    requiredWalletType === WalletType.EVM &&
+    userWallets.some((wallet) => wallet.chain === "EVM")
+  )
+    return true;
+  if (
+    requiredWalletType === WalletType.SOLANA &&
+    userWallets.some((wallet) => wallet.chain === "SOL")
+  )
+    return true;
+  if (
+    requiredWalletType === WalletType.SUI &&
+    userWallets.some((wallet) => wallet.chain === "SUI")
+  )
+    return true;
+  return false;
+};
+
+export const useDestinationWallet = (): Wallet | null => {
+  const userWallets = useDynamicUserWallets();
+  const requiredWalletType = useDestinationChain().walletType;
+  if (requiredWalletType === WalletType.EVM)
+    return userWallets.find((wallet) => wallet.chain === "EVM") || null;
+
+  if (
+    requiredWalletType === WalletType.SOLANA &&
+    userWallets.some((wallet) => wallet.chain === "SOL")
+  )
+    return userWallets.find((wallet) => wallet.chain === "SOL") || null;
+
+  if (
+    requiredWalletType === WalletType.SUI &&
+    userWallets.some((wallet) => wallet.chain === "SUI")
+  )
+    return userWallets.find((wallet) => wallet.chain === "SUI") || null;
+
+  return null;
+};
