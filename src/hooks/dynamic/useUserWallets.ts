2c21c27211f11499e4f561b5241e46d96d5e6130
elmoreshaw <elmoreshaws@gmail.com>
2025-10-11 20:03:31 +1100

    chore: cleaned up implementation of useUserWallets.ts

--- src/hooks/dynamic/useUserWallets.ts
+++ src/hooks/dynamic/useUserWallets.ts
@@ -6,61 +6,31 @@ import { useSourceChain, useDestinationChain } from "@/store/web3Store";
 import { WalletType } from "@/types/web3";
 
 export const useConnectedRequiredWallet = (): boolean => {
-  const userWallets = useDynamicUserWallets();
   const requiredWalletType = useSourceChain().walletType;
-  if (
-    requiredWalletType === WalletType.EVM &&
-    userWallets.some((wallet) => wallet.chain === "EVM")
-  )
-    return true;
-  if (
-    requiredWalletType === WalletType.SOLANA &&
-    userWallets.some((wallet) => wallet.chain === "SOL")
-  )
-    return true;
-  if (
-    requiredWalletType === WalletType.SUI &&
-    userWallets.some((wallet) => wallet.chain === "SUI")
-  )
-    return true;
-  return false;
+  return useWalletByType(requiredWalletType) !== null;
 };
 
 export const useSourceWallet = (): Wallet | null => {
-  const userWallets = useDynamicUserWallets();
   const requiredWalletType = useSourceChain().walletType;
-  if (requiredWalletType === WalletType.EVM)
+  return useWalletByType(requiredWalletType);
+};
+
+export const useDestinationWallet = (): Wallet | null => {
+  const requiredWalletType = useDestinationChain().walletType;
+  return useWalletByType(requiredWalletType);
+};
+
+export const useWalletByType = (walletType: WalletType): Wallet | null => {
+  const userWallets = useDynamicUserWallets();
+  if (walletType === WalletType.EVM)
     return userWallets.find((wallet) => wallet.chain === "EVM") || null;
-  if (
-    requiredWalletType === WalletType.SOLANA &&
-    userWallets.some((wallet) => wallet.chain === "SOL")
-  )
+  if (walletType === WalletType.SOLANA)
     return userWallets.find((wallet) => wallet.chain === "SOL") || null;
-  if (
-    requiredWalletType === WalletType.SUI &&
-    userWallets.some((wallet) => wallet.chain === "SUI")
-  )
+  if (walletType === WalletType.SUI)
     return userWallets.find((wallet) => wallet.chain === "SUI") || null;
   return null;
 };
 
-export const useDestinationWallet = (): Wallet | null => {
-  const userWallets = useDynamicUserWallets();
-  const requiredWalletType = useDestinationChain().walletType;
-  if (requiredWalletType === WalletType.EVM)
-    return userWallets.find((wallet) => wallet.chain === "EVM") || null;
-
-  if (
-    requiredWalletType === WalletType.SOLANA &&
-    userWallets.some((wallet) => wallet.chain === "SOL")
-  )
-    return userWallets.find((wallet) => wallet.chain === "SOL") || null;
-
-  if (
-    requiredWalletType === WalletType.SUI &&
-    userWallets.some((wallet) => wallet.chain === "SUI")
-  )
-    return userWallets.find((wallet) => wallet.chain === "SUI") || null;
-
-  return null;
+export const useIsWalletTypeConnected = (walletType: WalletType): boolean => {
+  return useWalletByType(walletType) !== null;
 };
