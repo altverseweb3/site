50fe7baf0fefc424faa88df4cbcd37c03b2f76e2
elmoreshaw <elmoreshaws@gmail.com>
2025-10-14 08:38:37 +1100

    bugfix: attempting to resolve issue with slow builds

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    // Ignore React Native specific dependencies that MetaMask SDK tries to import
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@react-native-async-storage/async-storage": false,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
